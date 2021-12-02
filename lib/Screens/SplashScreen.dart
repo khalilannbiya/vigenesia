2b6133fbff579f44d8a1106b203878b87ad72e80
khalilannbiya <syeichkhalil30@gmail.com>
2021-12-02 16:47:20 +0700

    merubah duraion splash screen

--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -15,7 +15,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   }
 
   startSplashScreen() async {
-    var duration = const Duration(seconds: 5);
+    var duration = const Duration(seconds: 4);
     return Timer(duration, () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (_) {
