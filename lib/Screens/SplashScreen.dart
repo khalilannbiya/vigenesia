226959a1d6987835483486ef594869badca8209c
khalilannbiya <syeichkhalil30@gmail.com>
2021-12-02 10:40:16 +0700

    menambahkan splash screen

---
+++ lib/Screens/SplashScreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'Login.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return Login();
+        }),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xf9f9f9),
+      body: Center(
+        child: Image.asset("images/logo.png", width: 150.0),
+      ),
+    );
+  }
+}
