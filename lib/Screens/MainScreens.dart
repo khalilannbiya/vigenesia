e8ed429ea084cb6cf074405fc67855341690b524
khalilannbiya <syeichkhalil30@gmail.com>
2021-12-13 18:19:27 +0700

    menghapus login.dart dan register.dart

--- lib/Screens/MainScreens.dart
+++ lib/Screens/MainScreens.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:vigenesia/Screens/LoginPage.dart';
-import 'Login.dart';
 import 'package:vigenesia/Constant/const.dart';
 import 'package:another_flushbar/flushbar.dart';
 
